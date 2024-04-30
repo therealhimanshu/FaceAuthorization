08dbeee852f02b213402db9dd962beb8f184cd73
Himanshu Joshi <102641944+therealhimanshu@users.noreply.github.com>
2024-04-30 20:53:36 +0530

    Update tryfacerec.py
    Removed the commented serial code used for sending signal to arduino, now converted to a basic use facial authorization python code

--- tryfacerec.py
+++ tryfacerec.py
@@ -2,9 +2,7 @@ import cv2
 import os
 import face_recognition
 import numpy as np
-import cvzone
 import pickle
-from serial import Serial
 import time
 
 cap = cv2.VideoCapture(0)
@@ -13,18 +11,6 @@ cap.set(4, 480)
 
 imgBackground = cv2.imread('resources/bg-img.png')
 
-# ser = Serial('COM3', 9600)
-
-buffer = 0
-
-# def send_signal(signal):
-#     global buffer
-#     ser.write(signal.encode())  # Convert the signal to bytes and write it to the serial port
-#     buffer += 1
-#     if buffer >= 10:  # Send signal every 10 loops
-#         buffer = 0
-
-# Importing the mode images into a list
 folderModePath = 'resources/Modes'
 modePathList = os.listdir(folderModePath)
 imgModeList = []
@@ -32,13 +18,12 @@ imgModeList = []
 for path in modePathList:
     imgModeList.append(cv2.imread(os.path.join(folderModePath, path)))
 
-# Load the encoding file
-# print("Loading Encoding File...")
+
 file = open('EncodeFile.p', 'rb')
 encodeListKnownWithNames = pickle.load(file)
 file.close()
 encodeListKnown, person_name = encodeListKnownWithNames
-# print("Encoding File Loaded")
+
 
 modeType = 0
 counter = 0
@@ -47,9 +32,9 @@ id = -1
 
 signal = '0'
 
-start_time = time.time()  # Record the start time
+start_time = time.time()  
 
-while time.time() - start_time <= 10:  # Run the loop for 10 seconds
+while time.time() - start_time <= 10: 
     success, img = cap.read()
 
     imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
@@ -72,10 +57,7 @@ while time.time() - start_time <= 10:  # Run the loop for 10 seconds
         matchIndex = np.argmin(faceDis)
         if matches[matchIndex] and faceDis[matchIndex] < 0.5:
             y1, x2, y2, x1 = faceLoc
-            # y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
-            # bbox = 60 + x1, 240 + y1, x2 - x1, y2 - y1
             color = (0, 255, 0)
-            # imgBackground = cvzone.cornerRect(imgBackground, bbox,colorC=(0, 255, 0),colorR=(0, 255, 0), rt=0, t=3)
             cv2.rectangle(imgBackground, (x1*4+70, y1*4+120), (x2*4+70, y2*4+120), color, 2)
             signal = '1'
             id = matchIndex
@@ -83,16 +65,11 @@ while time.time() - start_time <= 10:  # Run the loop for 10 seconds
                     cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
             
             if(time.time()- start_time>5):
-                # send_signal(signal)
-                print("Sending recognized signal to the arduino!")
+                print("Recognized")
                 cap.release()
                 cv2.destroyAllWindows()
-                # ser.close()
                 exit()
         else:
-            #print(matches[matchIndex], faceDis[matchIndex])
-            # Face not authorized
-            #print("Sending unrecognized signal to the arduino!")
             color = (0,0,255)            
             y1, x2, y2, x1 = faceLoc
             y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
@@ -105,6 +82,4 @@ while time.time() - start_time <= 10:  # Run the loop for 10 seconds
 
 cap.release()
 cv2.destroyAllWindows()
-print("Unrecognized Signal is sent to the Arduino!")
-# send_signal('0')  # Send '0' to the Arduino after 10 seconds
-# ser.close()
+print("Unrecognized")
